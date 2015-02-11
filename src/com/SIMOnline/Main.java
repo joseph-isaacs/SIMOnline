1ef118db7543bcf5b18b22dc5a8b529d8ad516ae
Curdled <joe.isaacs@live.co.uk>
2015-02-11 13:37:34 +0000

    Adding a main file and hello world method

---
+++ src/com/SIMOnline/Main.java
@@ -0,0 +1,10 @@
+package com.SIMOnline;
+
+/**
+ * Created by joeisaacs on 11/02/2015.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello, SIMOnline");
+    }
+}
