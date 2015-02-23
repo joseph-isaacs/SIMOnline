07bd8f3dbb2e0b1d820cb05fd8262f73f8866e98
Curdled <joe.isaacs@live.co.uk>
2015-02-23 16:45:16 +0000

    Added some inital drawing code

--- src/uk/co/matdev/SIMOnline/core/WorldMangager.java
+++ src/uk/co/matdev/SIMOnline/core/WorldMangager.java
@@ -2,23 +2,27 @@ package uk.co.matdev.SIMOnline.core;
 
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
+import uk.co.matdev.SIMOnline.maths.Vector2d;
 
 /**
  * Created by joeisaacs on 23/02/2015.
  */
 public class WorldMangager implements SIMObject{
 
-    private int width = 100;
-    private int height = 100;
+    private Vector2d<Integer> mWorldSize = new Vector2d<>(80,80);
 
-    private int tileSize = 10;
+
+
+    private int mTileSize = 10;
 
     @Override
     public void draw(Graphics g) {
-        for (int i = 0; i < width; i++) {
-            for(int j = 0; j < height; j++){
-                g.setColor(new Color(0,255,0,i*j % 255));
-                g.fillRect(i*tileSize, j*tileSize, tileSize, tileSize);
+        g.setColor(new Color(20,20,20,255));
+        g.fillRect(0,0,(mWorldSize.getX()+10)* mTileSize, (10+ mWorldSize.getY())* mTileSize);
+        for (int i = 0; i < mWorldSize.getX(); i++) {
+            for(int j = 0; j < mWorldSize.getY(); j++){
+                g.setColor(Color.green);
+                g.fillRect(i* mTileSize + i, j* mTileSize + j, mTileSize, mTileSize);
             }
         }
     }
