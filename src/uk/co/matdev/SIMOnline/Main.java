c2d79f954d2e9f4ead7a25195960262315fa22c7
Curdled <joe.isaacs@live.co.uk>
2015-02-23 14:21:42 +0000

    Added rendering to main

--- src/uk/co/matdev/SIMOnline/Main.java
+++ src/uk/co/matdev/SIMOnline/Main.java
@@ -1,10 +1,52 @@
 package uk.co.matdev.SIMOnline;
 
-/**
- * Created by joeisaacs on 11/02/2015.
- */
-public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello, SIMOnline");
+
+
+
+
+import org.lwjgl.opengl.Display;
+import org.newdawn.slick.*;
+import org.newdawn.slick.Color;
+import uk.co.matdev.SIMOnline.core.ObjectMangager;
+
+
+public class Main extends BasicGame {
+    public static int WIDTH = 1200;
+    public static int HEIGHT = 1000;
+    public static final boolean FULLSCREEN = false;
+
+    private ObjectMangager om = new ObjectMangager();
+
+    public static final String TITLE = "SlickTiled Testing Canvas";
+
+    public Main(String name) {
+        super(name);
     }
+
+    @Override
+    public void init(GameContainer gameContainer) throws SlickException {
+
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, int i) throws SlickException {
+
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, org.newdawn.slick.Graphics graphics) throws SlickException {
+        om.render(graphics);
+    }
+
+    public static void main(String args[]) throws SlickException {
+        Display.setResizable(true);
+
+        AppGameContainer app = new AppGameContainer(new Main("Hello"));
+
+        app.setDisplayMode(800, 600, false);
+        app.start();
+    }
+
+
+
 }
