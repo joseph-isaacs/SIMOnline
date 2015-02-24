60a96d6df851760cc6f261b5d38439382b2b25fe
Curdled <joe.isaacs@live.co.uk>
2015-02-24 13:20:00 +0000

    Added some by ideas behind these changes

--- src/uk/co/matdev/SIMOnline/core/GraphicsDrawer.java
+++ src/uk/co/matdev/SIMOnline/core/GraphicsDrawer.java
@@ -6,6 +6,12 @@ import org.newdawn.slick.Graphics;
  * Created by joeisaacs on 24/02/2015.
  */
 public class GraphicsDrawer {
+    //This class will own the GraphicsImp?
+    //Don't know if this is better or other class should just have
+    //access to GraphicsImp directly.
+
+
+    //could added a method the check if a class is drawing outside of it's bounds.
     GraphicsImp mImp;
 
     GraphicsDrawer(GraphicsImp imp){
--- src/uk/co/matdev/SIMOnline/core/GraphicsImp.java
+++ src/uk/co/matdev/SIMOnline/core/GraphicsImp.java
@@ -4,5 +4,6 @@ package uk.co.matdev.SIMOnline.core;
  * Created by joeisaacs on 24/02/2015.
  */
 public interface GraphicsImp {
+    //specific classes will implements this interface meaning that the graphics backend can be swapped out.
     void drawRectangle(float x, float y,float width, float height);
 }
