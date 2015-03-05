9a95e71d1f32f14bf058222373c868eb2ed1a089
Curdled <joe.isaacs@live.co.uk>
2015-03-05 12:32:48 +0000

    The data structure for holding all element of a battlefield

--- src/uk/co/matdev/SIMOnline/core/BattlefieldManager.java
+++ src/uk/co/matdev/SIMOnline/core/BattlefieldManager.java
@@ -3,6 +3,7 @@ package uk.co.matdev.SIMOnline.core;
 
 import uk.co.matdev.SIMOnline.maths.Vector2d;
 
+import java.util.ArrayList;
 
 
 /**
@@ -10,10 +11,13 @@ import uk.co.matdev.SIMOnline.maths.Vector2d;
  */
 public class BattlefieldManager implements SIMObject{
 
+
+    private int[][] mUnitPositions;
     private Vector2d<Integer> mWorldSize = new Vector2d<>(20,20);
     private int mTileSize = 10;
 
     BattlefieldManager(){
+        mUnitPositions = new int[mWorldSize.getX()][mWorldSize.getY()];
     }
 
     BattlefieldManager(Vector2d<Integer> ObjectLocation){
