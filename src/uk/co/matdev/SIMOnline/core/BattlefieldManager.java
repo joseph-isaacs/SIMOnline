4e08a21ac6e8ef1d8bbb82d149f786c0b228b12c
Curdled <joe.isaacs@live.co.uk>
2015-03-05 15:16:03 +0000

    first go at unit collision

--- src/uk/co/matdev/SIMOnline/core/BattlefieldManager.java
+++ src/uk/co/matdev/SIMOnline/core/BattlefieldManager.java
@@ -1,9 +1,8 @@
 package uk.co.matdev.SIMOnline.core;
 
-
 import uk.co.matdev.SIMOnline.maths.Vector2d;
 
-import java.util.ArrayList;
+import java.util.Random;
 
 
 /**
@@ -11,14 +10,26 @@ import java.util.ArrayList;
  */
 public class BattlefieldManager implements SIMObject{
 
-
-    private int[][] mUnitPositions;
+    private SIMUnit[][] mUnitPositions;
     private Vector2d<Integer> mWorldSize = new Vector2d<>(20,20);
     private int mTileSize = 10;
 
+    /**
+     * will create a matrix
+     * M[0][1]
+     *  - -
+     * | | |
+     * |X| |
+     *
+     *  M[1][0]
+     *  - -
+     * | |X|
+     * | | |
+     */
     BattlefieldManager(){
-        mUnitPositions = new int[mWorldSize.getX()][mWorldSize.getY()];
+        mUnitPositions = new SIMUnit[mWorldSize.getX()][mWorldSize.getY()];
     }
+
 
     BattlefieldManager(Vector2d<Integer> ObjectLocation){
     }
@@ -29,6 +40,45 @@ public class BattlefieldManager implements SIMObject{
 
     @Override
     public void doUpdate() {
-        ;
+        for (int x = 0; x != mUnitPositions.length; x++) {
+            for (int y = 0; y != mUnitPositions[x].length; y++) {
+                SIMUnit unit = mUnitPositions[x][y];
+                if(unit != null){
+                    updateHelper(unit, x, y, false);
+                }
+            }
+        }
+    }
+
+    private void updateHelper(SIMUnit unit, int x, int y, boolean hitOnce){
+
+        int newXPos = x + unit.getTargetVelocity().getX();
+        int newYPos = y + unit.getTargetVelocity().getY();
+        SIMUnit otherUnit = mUnitPositions[newXPos][newYPos];
+
+        if (!moveUnit(unit, x, y, newXPos, newYPos)) {
+            Vector2d<Integer> afterVelocity = unit.collision(otherUnit);
+            newXPos = x + afterVelocity.getX();
+            newYPos = y + afterVelocity.getY();
+            if(!moveUnit(unit, x,y,newXPos,newYPos)){
+                Vector2d<Integer> afterVelocity2 = unit.collision(otherUnit);
+                newXPos = x + afterVelocity2.getX();
+                newYPos = y + afterVelocity2.getY();
+                if(!moveUnit(unit, x, y, newXPos, newYPos))
+                    return;
+            }
+
+        }
+    }
+
+    private boolean moveUnit(SIMUnit unit, int x, int y, int newXPos, int newYPos){
+        if(mUnitPositions[newXPos][newYPos] == null) {
+            mUnitPositions[newXPos][newYPos] = unit;
+            mUnitPositions[x][y] = null;
+            return true;
+        }
+        else if(mUnitPositions[newXPos][newYPos].equals(unit))
+            return true;
+        return false;
     }
 }
